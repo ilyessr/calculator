4b461dc273d293b1e1c01a7d703edc203fe9e498
Ilyess <rachedi.ilyess@gmail.com>
2025-08-06 22:29:12 +0200

    feat: added buttons

--- script.js
+++ script.js
@@ -1,3 +1,22 @@
+const buttons = [
+  "7",
+  "8",
+  "9",
+  "/",
+  "4",
+  "5",
+  "6",
+  "*",
+  "1",
+  "2",
+  "3",
+  "-",
+  "0",
+  ".",
+  "=",
+  "+",
+];
+
 function add(a, b) {
   return a + b;
 }
@@ -14,3 +33,37 @@ function divide(a, b) {
   if (b === 0) return null;
   return a / b;
 }
+
+function operate(operator, a, b) {
+  switch (operator) {
+    case "+":
+      return add(a, b);
+    case "-":
+      return subtract(a, b);
+    case "*":
+      return multiply(a, b);
+    case "/":
+      return divide(a, b);
+    default:
+      return null;
+  }
+}
+
+function initiateCalculator() {
+  const buttonsContainer = document.querySelector(".buttons");
+
+  buttons.forEach((symbol) => {
+    const button = document.createElement("button");
+    button.textContent = symbol;
+    button.classList.add("btn");
+    button.setAttribute("data-value", symbol);
+    buttonsContainer.appendChild(button);
+  });
+
+  const clearButton = document.createElement("button");
+  clearButton.textContent = "Clear";
+  clearButton.classList.add("clear");
+  buttonsContainer.appendChild(clearButton);
+}
+
+initiateCalculator();
